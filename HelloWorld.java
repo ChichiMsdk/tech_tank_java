f8dc2a97b9e9a8a58fcd3b242c80e890910cdcdb
Chihab <chihab.moussadyk@gmail.com>
2023-11-13 13:39:52 +0100

    added addressBook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
+		if (args.length < 2)
+		{
+			System.out.println("Usage: java HelloWorld <example1> <example2>");
+			return;
+		}
         System.out.println("Hello, World!");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
