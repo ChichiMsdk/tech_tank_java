0f602761e78a06029f096ffc9b93ddb12b22bade
Chihab <chihab.moussadyk@gmail.com>
2023-11-13 21:24:52 +0100

    getting it

--- AddressBook.java
+++ AddressBook.java
@@ -3,15 +3,13 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.Comparator;
 
 public class AddressBook {
+
     public static void main(String[] args) {
 		if (args.length != 1) {
-			System.out.println("Usage: java HelloWorld <filename>");
+			System.out.println("Usage: java HelloWorld <filename> <name1>"+ 
+					"<name2> names are optional");
 			return;
 		}
 		List<People> person = new ArrayList<>();
@@ -25,24 +23,33 @@ public class AddressBook {
 		catch (IOException e) {
 			System.out.println("Error reading file: "
 					+ args[0] + " " + e.getMessage());
+			return;
 		}
-		List<DateComparison> dater = new ArrayList<>();
+		//trying to make everything in one loop
 		int maleCount = 0;
-		for (int i = 0; i < person.size(); i++) {
-			person.setRealAge();	
+		int femaleCount = 0;
+		int i;
+		People oldest = person.get(0);
+		for (i = 0; i < person.size(); i++) {
+			if (i > 0){
+				if (person.get(i).getDate().isBefore(person.get(i-1)
+							.getDate())){
+					oldest = person.get(i);
+				}
+			}
 			if (person.get(i).getMales() == 1){
 				maleCount++;
 			}
 		}
-		People oldest = person.get(0);
-		for (int i = 0; i < person.size(); i++) {
-			if (person.get(i).getAge().isBefore(oldest.getAge()) == 1){
-				oldest = person.get(i);
-			}
-		}
-		System.out.println("The oldest person is " + oldest.getFullName());
-		System.out.println("There are " + maleCount + " male(s)");
-		System.out.println(dater.get(0).getDate());
-		dater.get(0).getDiff(dater.get(0).getDate(), dater.get(1).getDate());
+		femaleCount = i - maleCount;
+		printAnswers(femaleCount, maleCount, oldest);
     }
+
+	public static void printAnswers(int femaleCount, int maleCount, 
+			People oldest){
+		System.out.println("The oldest person is " + oldest.getFullName());
+		System.out.println("There are " + maleCount + " male(s) and "
+				+ femaleCount + " female(s)");
+
+	}
 }
--- People.java
+++ People.java
@@ -1,22 +1,17 @@
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Comparator;
 
 public class People {
-
 	private String name;
 	private String lastName;
 	private String sex;
 	private String age;
-	private	LocalDate realAge;
+	private	LocalDate date;
 	private int	gender;
 	private String fullName;
+
 	public People (String fullName, String sex, String age){
 		this.fullName = fullName;
 		String[] nameParts = fullName.split(" ");
@@ -24,31 +19,30 @@ public class People {
 		this.lastName = nameParts[1];
 		this.sex = sex.split(" ")[1];
 		this.age = age.split(" ")[1];
+		setRealAge();
 		this.gender = changeSex();
 	}
 	public int getMales(){
 		return gender;
 	}
-	//maybe this is faster to compare rather than strings every time? idk
 	public int changeSex(){
 		if (sex.compareTo("Male") == 0)
+		{
+			sex = "male";
 			return 1;
+		}
 		if (sex.compareTo("Female") == 0)
+		{
+			sex = "female";
 			return 2;
+		}
 		else
+		{
+			sex = "N/A";
 			return 0;
-	}
-	public void	setRealAge(){
-		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yy");
-		try{
-			realAge = LocalDate.parse(age, format);
 		}
-		catch (DateTimeParseException e){
-			System.out.println("Error parsing date: " + e.getMessage());
-			return;
-		}
-		realAge = parseDate(realAge);
 	}
+
 	private static LocalDate parseDate(LocalDate date){
 		if (date == null)
 			return null;
@@ -56,8 +50,21 @@ public class People {
 			date = date.minusYears(100);
 		return date;
 	}
-	public LocalDate getRealAge(){
-		return realAge;
+
+	private	void	setRealAge(){
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yy");
+		try{
+			date = LocalDate.parse(age, format);
+		}
+		catch (DateTimeParseException e){
+			System.out.println("Error parsing date: " + e.getMessage());
+			return;
+		}
+		date = parseDate(date);
+	}
+
+	public LocalDate getDate(){
+		return date;
 	}
 	public String getFullName(){
 		return fullName;
